596a27e5e96634c130e53491fc179273a42e2afb
Antoine FRESSE <afresse54@gmail.com>
2014-11-18 16:58:08 +0100

    Prefab lumiere + script dans le noir

---
+++ Assets/Scripts/AfraidOfTheDark.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AfraidOfTheDark : MonoBehaviour {
+
+
+    public RenderTexture LightMap;
+    public Transform Fille;
+    public Camera GameCamera;
+
+    public bool CanMove { get; private set; }
+
+    // Use this for initialization
+	void Start () {
+	    CanMove = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    var screenPosition = GameCamera.WorldToScreenPoint(Fille.position);
+        var tex = new Texture2D(LightMap.width, LightMap.height);
+	    RenderTexture.active = LightMap;
+        tex.ReadPixels(new Rect(0,0, LightMap.width, LightMap.height), 0,0);
+        tex.Apply();
+	    var lightIntensity = tex.GetPixel((int) screenPosition.x, (int) screenPosition.y).r;
+        CanMove = (lightIntensity > 0.1f);
+	}
+
+
+}
