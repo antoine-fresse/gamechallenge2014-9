a19f91fc83d60035c13ee93cbc5c210bdf36d906
mludwiczak <maryan.ludwiczak@gmail.com>
2014-11-18 13:09:57 +0100

    jump

--- Assets/Scripts/TestMovement.cs
+++ Assets/Scripts/TestMovement.cs
@@ -7,22 +7,25 @@ public class TestMovement : MonoBehaviour {
     public float Speed = 10f;
     private Transform _transform;
     private SpriteRenderer _renderer;
+    private Animator _animator;
 	// Use this for initialization
 	void Start () {
 	    _transform = transform;
 	    _renderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
 	}
 
     void Update() {
         //Camera.transform.position = new Vector3(_transform.position.x, 0f, -10f);
+
     }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         //Debug.Log(Input.GetAxis("Horizontal"));
 
-        _transform.position = new Vector3(_transform.position.x + Input.GetAxis("Horizontal") * Speed * Time.deltaTime / 128f, _transform.position.y, _transform.position.z + Input.GetAxis("Vertical") * Speed * Time.deltaTime * 1.6f / 128f);
+        _transform.parent.transform.position = new Vector3(_transform.position.x + Input.GetAxis("Horizontal") * Speed * Time.deltaTime / 128f, _transform.position.y, _transform.position.z + Input.GetAxis("Vertical") * Speed * Time.deltaTime * 1.6f / 128f);
 
-	    
+       
 	}
 }
