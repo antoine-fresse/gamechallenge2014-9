c09550493ed8bb37c8843bacc639078182c65296
mludwiczak <maryan.ludwiczak@gmail.com>
2014-11-18 18:53:14 +0100

    eau detruite a la premiere flamme

--- Assets/Scripts/collisionFire.cs
+++ Assets/Scripts/collisionFire.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class collisionFire : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other){
-		if (other.tag == "Eau")
+		if (other.tag == "Eau") {
 			Destroy (gameObject);
+			Destroy (other.gameObject);
+		}
 		if(other.tag == "Player")
 		{
 			Destroy(other.gameObject);
