38d4a81a42996834f1a7249c8db051962b75e417
Broopyn <Broopyn@bitbucket.org/Broopyn/rollingball.git>
2014-11-18 16:31:03 +0100

    Ajout World + script World

---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class World : MonoBehaviour {
+	
+	public bool isPapy;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
