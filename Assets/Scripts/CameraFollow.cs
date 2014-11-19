bf33eb5610ce6004db831eb91b52abf3973e3a66
mludwiczak <maryan.ludwiczak@gmail.com>
2014-11-19 06:38:55 +0100

    integration sound

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,18 +6,15 @@ public class CameraFollow : MonoBehaviour {
     private Transform ToFollow;
     public Transform FireWall;
     private Transform _transform;
+
     // Use this for initialization
 	void Start () {
 	    _transform = transform;
-		//ToFollow = w.l.transform;
+		ToFollow = GameObject.FindGameObjectWithTag ("MainCamera").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Network.peerType == NetworkPeerType.Server)
-        {
-            ToFollow = w.l.transform;
-            _transform.position = new Vector3(Mathf.Max(ToFollow.position.x, FireWall.position.x + .8f), _transform.position.y, _transform.position.z);
-        }
+            _transform.position = new Vector3(Mathf.Max(ToFollow.position.x, FireWall.position.x + .8f), _transform.position.y, _transform.position.z);   
 	}   
 }
