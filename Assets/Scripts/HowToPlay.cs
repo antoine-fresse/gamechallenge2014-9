da983e881216bb550ef10e6eb600d5bbb3a8d309
mludwiczak <maryan.ludwiczak@gmail.com>
2014-11-19 12:37:17 +0100

    How To Play

---
+++ Assets/Scripts/HowToPlay.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HowToPlay : MonoBehaviour {
+	public GameObject eau;
+	public GameObject lenka;
+	public GameObject papy;
+	public GameObject lumiere;
+
+	public void afficherLenka(){
+		eau.SetActive (false);
+		lenka.SetActive(true);
+	}
+
+	public void afficherPapy(){
+		lenka.SetActive (false);
+		papy.SetActive(true);
+	}
+
+	public void afficherLumiere(){
+		papy.SetActive (false);
+		lumiere.SetActive(true);
+	}
+
+	public void retourMenu(){
+		Application.LoadLevel ("Menu");
+	}
+}
