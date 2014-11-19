ff091fe3bd9215940f5364041dc1dfaa0ac45c6f
Antoine FRESSE <afresse54@gmail.com>
2014-11-19 06:50:35 +0100

    Correction du Camera Follow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -10,21 +10,16 @@ public class CameraFollow : MonoBehaviour {
     // Use this for initialization
 	void Start () {
 	    _transform = transform;
-<<<<<<< HEAD
 		ToFollow = GameObject.FindGameObjectWithTag ("MainCamera").transform;
-=======
+
 	    var l = GameObject.Find("Light(Clone)");
 	    if (l) 
 	        ToFollow = l.transform;
 	    
->>>>>>> 1295691f31ffc51c61d2b0c763ec66e86d8b6dcf
 	}
 	
 	// Update is called once per frame
 	void Update () {
-<<<<<<< HEAD
-            _transform.position = new Vector3(Mathf.Max(ToFollow.position.x, FireWall.position.x + .8f), _transform.position.y, _transform.position.z);   
-=======
 	    if (!ToFollow) {
             var l = GameObject.Find("Light(Clone)");
             if (l)
@@ -35,6 +30,6 @@ public class CameraFollow : MonoBehaviour {
 
         _transform.position = new Vector3(Mathf.Max(ToFollow.position.x, FireWall.position.x+0.4f), _transform.position.y, _transform.position.z);
         
->>>>>>> 1295691f31ffc51c61d2b0c763ec66e86d8b6dcf
+
 	}   
 }
