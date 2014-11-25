c54baf51c290e62140727d52e59177c987a1ecb8
Antoine FRESSE <afresse54@gmail.com>
2014-11-25 09:28:35 +0100

    Amélioration rendu

--- Assets/Scripts/BigFire.cs
+++ Assets/Scripts/BigFire.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class BigFire : MonoBehaviour {
 
-	public Transform smallFire;
-
+	public Transform SmallFire;
+	private bool _quitting = false;
+	void OnApplicationQuit() {
+		_quitting = true;
+	}
 	void OnDestroy() {
-		Instantiate(smallFire, transform.position, Quaternion.identity);
+		if(!_quitting)
+			Instantiate(SmallFire, transform.position, Quaternion.identity);
 	}
 }
